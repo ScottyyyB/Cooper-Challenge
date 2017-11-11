4d7b2956620ef33674d6cf9f92c8cb3048a4961e
ScottB <17sb@cis.dk>
2017-11-11 15:06:39 +0100

    Add Person runCooper function and test person.cooperStatus

--- spec/person_spec.js
+++ spec/person_spec.js
@@ -1,13 +1,18 @@
 describe("Person", function() {
   beforeEach(function() {
-    person = new Person({gender: 'female', age: 20});
+    person = new Person({gender: 'female', age: 13});
   });
 
   it("expects person's age to be 20", function() {
-    expect(person.age).toEqual(20);
+    expect(person.age).toEqual(13);
   });
 
   it("expects person's gender to be female", function() {
     expect(person.gender).toEqual('female');
+  });
+  it("expects person's cooper status to be Poor", function() {
+    person.runCooper();
+    spyOn(Math, 'random').and.returnValue(5);
+    expect(person.cooperStatus).toEqual("Poor");
   });
 });
--- src/person.js
+++ src/person.js
@@ -2,3 +2,13 @@ function Person(attr) {
   this.gender = attr.gender;
   this.age = attr.age;
 }
+
+Person.prototype.runCooper = function() {
+  if (this.gender == 'male') {
+    calculator = new MaleCalculator();
+    calculator.maleCooper(this);
+  } if (this.gender == 'female') {
+    calculator = new FemaleCalculator();
+    calculator.femaleCooper(this);
+  }
+};
